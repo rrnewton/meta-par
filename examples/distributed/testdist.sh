4bd39c4828757ed272362da52fa786355fb0d162
Ryan Newton <rrnewton@gmail.com>
2011-12-07 00:14:11 -0500

    Minor: added set -e to bash scripts

--- examples/distributed/testdist.sh
+++ examples/distributed/testdist.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
 N=$1
 shift
+
+# Error if any command fails:
+set -e 
+
 ghc -i../.. --make parfib_dist.hs -O2 -threaded -rtsopts -fforce-recomp $@
 cp parfib_dist worker/
 pushd worker
 ./parfib_dist monad $N +RTS -N2 -RTS & 
 popd
+
 time ./parfib_dist monad $N +RTS -N2 -RTS
+echo "Done running master computation.  Killing worker if it's still running."
 kill -9 $!
